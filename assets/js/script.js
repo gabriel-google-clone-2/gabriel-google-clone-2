aea7a5fe25ecd881a752ddee972dc95af35825fb
Gabriel Marques Correia <gabriel.correia@tecno.com.br>
2023-11-14 11:41:52 -0300

    Primeiro commit do google-clone

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+const menu = document.querySelector("#btn-menu")
+
+menu.addEventListener("click", () => {
+
+    console.log("Clicou")
+
+})
+
+const buttonImLuck = document.querySelector("#luck")
+
+luck.addEventListener("click", () => {
+
+    location.href = "https://www.google.com/logos/2010/pacman10-i.html"
+
+})
